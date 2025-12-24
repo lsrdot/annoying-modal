808ce2e928abdbe15f7657b5123669efef8242c9
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2025-12-24 20:41:53 -0300

    first commit

--- index.js
+++ index.js
@@ -5,45 +5,45 @@ const modalText = document.getElementById('modal-text')
 const declineBtn = document.getElementById('decline-btn')
 const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
-setTimeout(function(){
+setTimeout( () => {
     modal.style.display = 'inline'
 }, 1500)
 
-modalCloseBtn.addEventListener('click', function(){
+modalCloseBtn.addEventListener('click', function () {
     modal.style.display = 'none'
-}) 
+})
 
-declineBtn.addEventListener('mouseenter', function(){
+declineBtn.addEventListener('mouseenter', function () {
     modalChoiceBtns.classList.toggle('modal-btns-reverse')
-}) 
+})
 
-consentForm.addEventListener('submit', function(e){
+consentForm.addEventListener('submit', function (e) {
     e.preventDefault()
-    
+
     const consentFormData = new FormData(consentForm)
     const fullName = consentFormData.get('fullName')
-    
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
-        <img src="images/loading.svg" class="loading">
-        <p id="upload-text">Uploading your data to the dark web...</p>
-    </div>` 
-    
-    setTimeout(function(){
+        <img src="images/loading.svg" class="loading"> => 
+        <p id="upload-text">Storing your data in a malicious way...</p>
+    </div>`
+
+    setTimeout(function () {
         document.getElementById('upload-text').innerText = `
-        Making the sale...`
+        Saling your data...`
     }, 1500)
-    
-    
-    setTimeout(function(){
+
+
+    setTimeout(() => {
         document.getElementById('modal-inner').innerHTML = `
         <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
-        <p>We just sold the rights to your eternal soul.</p>
+        <p>We just sold the rights to your soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
         </div>
     `
-    modalCloseBtn.disabled = false
+        modalCloseBtn.disabled = false
     }, 3000)
-  
+
 }) 
